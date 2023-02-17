7425838fe493756a4479768e287480002e87bf50
joki20 <joki20@student.bth.se>
2023-02-17 15:20:03 +0000

    minor changes

--- src/index.tsx
+++ src/index.tsx
@@ -11,7 +11,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
     <BrowserRouter>
-        <HashRouter basename="/" >
+        <HashRouter basename="/xyz" >
             <App />
         </HashRouter>
     </BrowserRouter>
