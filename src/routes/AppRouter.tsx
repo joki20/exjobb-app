3b1bb7170bd4e7fd38129aef531b508925dcd5da
joki20 <joki20@student.bth.se>
2023-02-17 14:24:55 +0000

    hashrouter index.tsx

--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -6,24 +6,20 @@ import Logs from './../pages/Logs';
 import LogsDetail from './../pages/LogsDetail';
 import Info from '../pages/Info';
 import Notes from './../pages/Notes';
-//
-import { useLocation } from "react-router-dom";
 
 // props from App.tsx
-export default function AppRouter(props:any) {
-    let currentRoute = useLocation().pathname; // mandatory: <App> surrounded by <BrowserRouter> inside index.tsx
-    
+export default function AppRouter(props:any) {  
     // routes only accessible when logged in is true
     // destructure props within each page like this: {props}
     // otherwise you would have to do props.props.data to access data instead of props.data
     return (
         <div>
             <Routes>
-                <Route path="https://joki20.github.io" element={<Login props={props} />} />
-                {/* <Route path="/logs" element={<Logs props={props} />} />
+                <Route path="/" element={<Login props={props} />} />
+                <Route path="/logs" element={<Logs props={props} />} />
                 <Route path="/logs/:path" element={<LogsDetail props={props} />} />
                 <Route path="/info" element={<Info props={props} />} />
-                <Route path="/notes" element={<Notes props={props} />} /> */}
+                <Route path="/notes" element={<Notes props={props} />} />
             </Routes>
         </div>
     )
