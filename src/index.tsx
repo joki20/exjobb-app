becfb4417505cbbb7b88719a88d0c46a15c8f1b4
joki20 <joki20@student.bth.se>
2023-02-17 15:26:51 +0000

    minor changes

--- src/index.tsx
+++ src/index.tsx
@@ -10,11 +10,9 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-    <BrowserRouter>
-        <HashRouter basename="/xyz" >
-            <App />
-        </HashRouter>
-    </BrowserRouter>
+    <HashRouter>
+        <App />
+    </HashRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
