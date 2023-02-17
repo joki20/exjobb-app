215ec1c83085cb96fb69e962d530d0f8285f55f8
joki20 <joki20@student.bth.se>
2023-02-17 16:42:47 +0000

    minor changes

--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,26 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-
+// Här används ES5 Syntax för att webpack inte stödjer ES2015
 module.exports = {
-  entry: "./src/index.js",
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    publicPath: '/dist/',
-    filename: 'bundle.js'
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: path.join(__dirname, "public", "index.html"),
-    }),
-  ],
-  devServer: {
-    static: {
-      directory: path.join(__dirname, "build"),
+    // Webpack behöver veta var den ska börja, och det blir din applikations "main"-fil. 
+    //Den kollar på dina import-statements och samlar in allt som applikationen behöver för att köras...
+    entry: "./App.tsx",
+    output: {
+        // ... och stoppar det I en output-fil, som vi definierar här.
+        filename: "/build/bundle.js"
     },
-    port: 3000,
-  },
-  module: {
-    // exclude node_modules
-    rules: [
-      {
-        test: /\.(js)$/,
-        exclude: /node_modules/,
-        use: ["babel-loader"],
-      },
-    ],
-  },
-  // pass all js files through Babel
-  resolve: {
-    extensions: ["*", ".js", ".ts", ".tsx"],
-  }
+    module: {
+        loaders: [{
+            // Detta är ett reguljärt uttryck som identifierar alla dina .js-filer
+            test: /^(?!.*\.d\.tsx?$).*\.[tj]sx?$/g, 
+            // Vi vill inte kolla i node_modules, så vi exkluderar den mappen.
+            exclude: /node_modules/,
+            // Vi lägger till vår babel-loader...
+            loader: "babel",
+            // ... och berättar för den vilka presets vi vill använda.
+            query: {
+                presets: ['react', 'es2015']
+            }
+        }]
+    }
 };
+
+
